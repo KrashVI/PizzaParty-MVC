c94a22cc942967599ee689086c87588f765eb502
Kirsten Markley <markleyk@otc.edu>
2024-02-09 15:10:09 -0600

    Initial commit

---
+++ app/src/main/java/com/zybooks/pizzaparty/MainActivity.java
@@ -0,0 +1,63 @@
+package com.zybooks.pizzaparty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    public final int   SLICES_PER_PIZZA = 8;
+
+    private EditText mNumAttendEditText;
+    private TextView mNumPizzasTextView;
+    private RadioGroup mHowHungryRadioGroup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNumAttendEditText = findViewById(R.id.num_attend_edit_text);
+        mNumPizzasTextView = findViewById(R.id.num_pizzas_text_view);
+        mHowHungryRadioGroup = findViewById(R.id.hungry_radio_group);
+
+    }
+
+    public void calculateClick(View view)
+    {
+        Toast.makeText(this, "Toat", Toast.LENGTH_LONG).show();
+        // Get how many are attending the party
+        int numAttend;
+        try {
+            String numAttendStr = mNumAttendEditText.getText().toString();
+            numAttend = Integer.parseInt(numAttendStr);
+        }
+        catch (NumberFormatException ex) {
+            numAttend = 0;
+        }
+
+        // Get hunger level selection
+        int checkedId = mHowHungryRadioGroup.getCheckedRadioButtonId();
+        PizzaCalculator.HungerLevel hungerLevel = PizzaCalculator.HungerLevel.RAVENOUS;
+        if (checkedId == R.id.light_radio_button) {
+            hungerLevel = PizzaCalculator.HungerLevel.LIGHT;
+        }
+        else if (checkedId == R.id.medium_radio_button) {
+            hungerLevel = PizzaCalculator.HungerLevel.MEDIUM;
+        }
+
+        // Get the number of pizzas needed
+        PizzaCalculator calc = new PizzaCalculator(numAttend, hungerLevel);
+        int totalPizzas = calc.getTotalPizzas();
+
+        // Place totalPizzas into the string resource and display
+        String totalText = getString(R.string.total_pizzas_num, totalPizzas);
+        mNumPizzasTextView.setText(totalText);
+    }
+}
